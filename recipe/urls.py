0c163c599fdc24ee5319594f93178b86c6d83cb9
Austin Okonkwo <austinokonkwo1995@gmail.com>
2025-07-19 23:54:31 +0100

    changes

--- recipe/urls.py
+++ recipe/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.recipe, name="index"),
     path("create_recipe/", views.create_recipe, name="create_recipe"),
     path("success", views.success, name="success"),
+    path("recipes/<int:recipe_id>/", views.recipe_detail, name="recipe-detail"),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -3,14 +3,18 @@ from . models import Recipe
 from . forms import RecipeForm
 
 # Create your views here.
+
+
 def recipe(request):
     recipes = Recipe.objects.all()
     context = {"recipes": recipes}
     template = "recipes/index.html"
     return render(request, template, context)
 
+
 def success(request):
     return render(request, "recipes/success.html")
+
 
 def create_recipe(request):
     if request.method == "POST":
@@ -23,3 +27,13 @@ def create_recipe(request):
         template = "recipes/create_recipe.html"
         context = {"form": form}
         return render(request, template, context)
+
+
+def recipe_detail(request, recipe_id):
+    """Detail view for a recipe"""
+    recipe = Recipe.objects.get(id=recipe_id)
+    context = {
+        "recipe": recipe
+    }
+    template = "recipes/recipe_detail.html"
+    return render(request, template, context)
